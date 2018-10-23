6107378caf71265ec0ab740be5e57c3d0c0ec9d0
David Wood <dwood96x@gmail.com>
2018-10-23 10:44:55 -0400

    Started

--- Generic Collections/Program.cs
+++ Generic Collections/Program.cs
@@ -10,6 +10,65 @@ namespace Generic_Collections
     {
         static void Main(string[] args)
         {
+            var d = new MyDictionary();
+            try
+            {
+                Console.WriteLine(d["Cats"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            d["Cats"] = 42;
+            d["Dogs"] = 17;
+            Console.WriteLine($"{(int)d["Cats"]}, {(int)d["Dogs"]}");
+        }
+    }
+    struct KeyValue
+    {
+        public string key { get; }
+        public object value { get; }
+        public KeyValue(string _key,object _value)
+        {
+            key = _key;
+            value = _value;
+        }
+    }
+    class MyDictionary
+    {
+        private KeyValue[] keys = new KeyValue[50];
+        private int NumOfKeys = 0;
+
+        public object this[string key]
+        {
+            set
+            {
+                bool exist = false;
+                for (int i = 0; i < keys.Length; i++)
+                {
+                    if (keys[i].key == key)
+                    {
+                        keys[i] = new KeyValue(key, keys[i].value);
+                        exist = true;
+                    }
+                }
+                if (exist == false)
+                {
+                    keys[NumOfKeys] = new KeyValue(key, value);
+                    NumOfKeys += 1;
+                }
+            }
+            get
+            {
+                for (int i = 0; i < keys.Length; i++)
+                {
+                    if (keys[i].key == key)
+                    {
+                        return keys[i].value;
+                    }
+                }
+                throw new KeyNotFoundException();
+            }
         }
     }
 }
